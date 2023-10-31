792f280d58fee2232de5a90181c11bd39ddbab68
emerson cardoso <emerson.cardoso.trindade@gmail.com>
2023-10-31 12:09:08 -0300

    Service layer, component registration

---
+++ src/main/java/com/emerson/course/services/UserService.java
@@ -0,0 +1,28 @@
+package com.emerson.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.emerson.course.entities.User;
+import com.emerson.course.repositories.UserRepository;
+
+@Service
+public class UserService  {
+	
+	@Autowired
+	private UserRepository repository;
+	
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User finById(Long id) {
+		Optional<User> obj =  repository.findById(id);
+		return obj.get();
+	}
+	
+}
