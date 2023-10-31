655c40cbed55efb1ed79b1d5e8c69844056ae809
emerson cardoso <emerson.cardoso.trindade@gmail.com>
2023-10-31 14:11:35 -0300

    Order, Instant, ISO 8601 part 2

---
+++ src/main/java/com/emerson/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.emerson.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.emerson.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+	
+}
