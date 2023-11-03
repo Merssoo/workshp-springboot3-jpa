15eeab5dc32db672d64baf1aeb813d9669cf2422
emerson cardoso <emerson.cardoso.trindade@gmail.com>
2023-11-03 19:57:38 -0300

    Payment, one-to-one association

--- src/main/java/com/emerson/course/config/TestConfig.java
+++ src/main/java/com/emerson/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.emerson.course.entities.Category;
 import com.emerson.course.entities.Order;
 import com.emerson.course.entities.OrderItem;
+import com.emerson.course.entities.Payment;
 import com.emerson.course.entities.Product;
 import com.emerson.course.entities.User;
 import com.emerson.course.entities.enums.OrderStatus;
@@ -81,8 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
-		
-		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 	
 	
